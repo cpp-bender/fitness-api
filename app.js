e92cd3b7f6e4a9c4e284c2622d3c13cc874b2c6d
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-02-27 12:38:38 +0300

    feat: start app

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// routers
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(PORT, ()=>{
+    console.log(`app started at http://localhost:${PORT}`);
+});
