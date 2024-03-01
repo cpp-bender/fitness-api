7880cf8690a718c63831eab69c2a545ab055a33d
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-03-01 19:50:22 +0300

    feat: add my profiles

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const register = async(req, res) =>{
     const user = await User.create(req.body);
@@ -42,11 +43,23 @@ const login = async(req, res) =>{
         secure: NODE_ENV === "development" ? false : true,
         expires: new Date(Date.now() + parseInt(JWT_COOKIE) * 1000),
     })
-    .json({mess:"designing login func.", user})
-
+    .json({mess:"designing login func.", user, accessToken:token});
 };
+
+const profileDetails = async(req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, function(err, decoded) {
+        if(err){
+            return res.status(401).json({mess: "error occurred"});
+        }
+        // Here you can use the decoded token
+        res.status(200).json({mess: "profile details", decoded});
+    });
+};
+
 
 module.exports = {
     register,
     login,
+    profileDetails,
 };
--- routers/user.js
+++ routers/user.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const {register, login} = require('../controllers/authController');
+const {register, login, profileDetails} = require('../controllers/authController');
 
+// get requests
 router.get('/',(req, res) => {
     const name = req.body.name;
     res.send(name);
 });
+router.get('/myProfile', profileDetails);
 
+// post requests
 router.post('/createUser', register);
-
 router.post('/login', login);
-
 
 module.exports = router;
