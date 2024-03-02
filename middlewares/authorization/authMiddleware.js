0dd185777806d11b2d6935f88264ffdedc51cc8a
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-03-02 13:41:39 +0300

    chore: update authMiddlware

--- middlewares/authorization/authMiddleware.js
+++ middlewares/authorization/authMiddleware.js
@@ -1,21 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const getAccessToRoute = (req, res, next) => {
-    const token = req.headers.authorization.split(':')[1];
-    const SECRET_KEY = process.env.JWT_SECRET_KEY;
-    jwt.verify(token, SECRET_KEY, function(err, decoded) {
-        if(err){
-            return res.status(401).json({mess: "error occurred"});
-        }
-        req.user = {
-            id: decoded.id,
-            name: decoded.name,
-        } 
-    });
+  const token = req.headers.authorization.split(":")[1];
+  const SECRET_KEY = process.env.JWT_SECRET_KEY;
+  jwt.verify(token, SECRET_KEY, function (err, decoded) {
+    if (err) {
+      return res.status(401).json({ mess: "error occurred" });
+    }
+    req.user = {
+      id: decoded.id,
+      name: decoded.name,
+    };
+  });
 
-    next();
-}
+  next();
+};
 
 module.exports = {
-    getAccessToRoute,
-}
+  getAccessToRoute,
+};
